48451d61dd482360a13ea44d47b301dcd6035ceb
Oluwateniola Adegbulugbe <adegbulugbe.teniola@outlook.com>
2022-12-22 21:45:15 +0100

    DSA - ALX Interviews
    Readme for repo & directory and Pascal's Triangle script.

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+A module on pascal triangle
+"""
+
+
+def pascal_triangle(n):
+    """
+    A function computing pascal triangle into
+    p_tri of p_tris
+    Args:
+        n (int)
+    Returns:
+        p_tri
+    """
+    p_tri = []
+    if n <= 0:
+        return p_tri
+    else:
+        for i in range(n):
+            p_tri.append([])
+            p_tri[i].append(1)
+            for j in range(1, i):
+                p_tri[i].append(p_tri[i - 1][j - 1] + p_tri[i - 1][j])
+            if(i != 0):
+                p_tri[i].append(1)
+    return p_tri
---
+++ 0x00-pascal_triangle/README.md
@@ -0,0 +1,9 @@
+# Pascal's Triangle
+> Each file in this repository holds code that illustrates Pascal's Triangle
+> specific to Data Structures and Algorithms.
+
+## Description of what each file shows:
+* 0-pascal_triangle.py: A function `def pascal_triangle(n):` that returns a list of lists of integers representing the Pascal’s triangle of `n`:
+
+	- Returns an empty list if `n <= 0`
+	- You can assume `n` will be always an integer
